1bb26ac8122e01ca34763608a981a65d88973ebf
deepakd6 <38915794+deepakd6@users.noreply.github.com>
2018-06-28 19:51:05 +0530

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -9,5 +9,5 @@ done
 docker build -f dockerfile_mysql -t sql_docker .    
 docker build -f dockerfile_app -t app_docker .   
 docker run -d --name=sql_image sql_docker      
-docker run -p 8080:8080 --name spring-app --link sql_image:mysql -d app_docker
+docker run -p 8080:8080 --name spring-apps --link sql_image:mysql -d app_docker
 docker ps
